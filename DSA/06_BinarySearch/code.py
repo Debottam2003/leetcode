4d26827297491bca9a26afaa34594ae2673da242
Debottam Kar <debottamkar2003@gmail.com>
2026-05-17 08:34:37 +0530

    working

---
+++ DSA/06_BinarySearch/code.py
@@ -0,0 +1,18 @@
+def binarySearch(arr, target):
+    low = 0
+    high = len(arr) - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            low = mid + 1
+        else:
+            high = mid - 1
+
+    return -1
+
+print(binarySearch([1, 2, 3, 4, 5], 3))  # Output: 2
+print(binarySearch([1, 2, 3, 4, 5], 6))  # Output: -1
